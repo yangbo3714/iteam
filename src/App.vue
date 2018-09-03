9dbe6876a92f46475eaf74ccda7d664946d46941
ddyang <963574947@qq.com>
2018-09-03 19:35:30 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+
+<script>
+// import vueElement from 'element-ui'
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
